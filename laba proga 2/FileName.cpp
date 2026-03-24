3769a26e54a024c42c081db4bf4bd4ba27779003
Frolly <267678762+dimabalabanov939-arch@users.noreply.github.com>
2026-03-24 23:52:40 +0300

    fixed output

--- laba proga 2/FileName.cpp
+++ laba proga 2/FileName.cpp
@@ -3,10 +3,9 @@
 
 using namespace std;
 
-class Groups
+struct Groups
 {
-public:
-	string NameGroupe;
+	char NameGroupe[100];
 	int unsigned quantity;
 };
 void CheckCin()
@@ -17,13 +16,9 @@ void CheckCin()
 		cin.clear();
 		cin.ignore(10000000, '\n');
 	}
-	else
-	{
-
-	}
 }
 
-void AddGroupe(Groups*& ArrGroupe, int& size)
+void AddGroupe(Groups*& ArrGroupe, int &size)
 {
 	Groups* temp_mas = new Groups[size + 1];
 
@@ -34,16 +29,23 @@ void AddGroupe(Groups*& ArrGroupe, int& size)
 
 	cout << "\nEnter the name groupe: ";
 	cin.ignore(10000000, '\n');
-	getline(cin, temp_mas[size].NameGroupe);
+	cin.getline(temp_mas[size].NameGroupe, 100);
 	cout << "\nEnter quantity students  (quantity < 30!!!): ";
-	while(!(cin >> temp_mas[size].quantity))
-	    {
-		CheckCin();
-		}
-	while(temp_mas[size].quantity > 30)
+	cin >> temp_mas[size].quantity;
+	while (cin.fail() || temp_mas[size].quantity > 30 || temp_mas[size].quantity <= 0) 
 	{
-	    cout << "Error!!! Quantity cannot exceed 30!!! Enter quantity students again: ";
+		if (cin.fail()) {
+			cout << "Error! Please enter a number! ";
+		}
+		else if (temp_mas[size].quantity > 30) {
+			cout << "Error!!! Quantity cannot exceed 30!!! ";
+		}
+		else if (temp_mas[size].quantity <= 0) {
+			cout << "Error!!! Quantity must be positive!!! ";
+		}
+		cout << "Enter quantity students again: ";
 		cin >> temp_mas[size].quantity;
+		CheckCin();
 	}
 
 
@@ -54,7 +56,7 @@ void AddGroupe(Groups*& ArrGroupe, int& size)
 	size ++;
 	cout << "Group added successfully!\n";
 }
-void EnterDataManually(Groups *&ArrGroupe, int& size)
+void EnterDataManually(Groups*& ArrGroupe, int &size)
 {
 	bool Flag = false;
 	while (Flag == false)
@@ -74,12 +76,38 @@ void EnterDataManually(Groups *&ArrGroupe, int& size)
 			break;
 		default:
 			cout << "Invalid choice! Please enter 1 or 2." << endl;
-
 		}
 	}
 }
+void OutputInConsole(Groups *ArrGroupe, int &size)
+{
+	cout << "1. Output all" << endl;
+	cout << "2. Output one" << endl;
+	cout << "Enter action number: " << endl;
+	int k;
+	int n = 0;
+	cin >> k;
+	CheckCin();
 
-void menu1(Groups *ArrGroupe, int size)
+	switch (k)
+	{
+	case 1:
+		for (int i = 0; i < size; i++)
+		{
+			cout << i + 1 << ". Name groupe: " <<  ArrGroupe[i].NameGroupe << "\n Quantity students: " << ArrGroupe[i].quantity << endl;
+		}
+		break;
+	case 2:
+		cout << "\nEnter number group: ";
+		cin >> n;
+		CheckCin();
+		cout << n << ". Name groupe: " << ArrGroupe[n - 1].NameGroupe << "\n Quantity students: " << ArrGroupe[n - 1].quantity << endl;
+		break;
+	default:
+		cout << "Invalid choice! Please enter 1 or 2." << endl;
+	}
+}
+void menu1(Groups*& ArrGroupe, int &size)
 {
 	cout << "======================================" << endl;
 	cout << "=              CHOISE                =" << endl;
@@ -104,7 +132,35 @@ void menu1(Groups *ArrGroupe, int size)
 		cout << "Invalid choice! Please enter 1, 2 or 3." << endl;
 	}
 }
-void menu(Groups *ArrGroupe, int size)
+void menu2(Groups* ArrGroupe, int &size)
+{
+	cout << "======================================" << endl;
+	cout << "=              CHOISE                =" << endl;
+	cout << "======================================" << endl;
+	cout << "1. Output the data in console" << endl;
+	cout << "2. Output data in a .txt file" << endl;
+	cout << "3. Output data in a .bin file" << endl;
+	cout << "Enter action number: " << endl;
+	int k;
+	cin >> k;
+	CheckCin();
+	switch (k)
+	{
+	case 1:
+		OutputInConsole(ArrGroupe, size);
+		cout << "\nPress Enter to continue...";
+		cin.ignore(10000000, '\n');
+		cin.get();
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	default:
+		cout << "Invalid choice! Please enter 1, 2 or 3." << endl;
+	}
+}
+void menu(Groups *&ArrGroupe, int &size)
 {
 	while (1)
 	{
@@ -126,16 +182,13 @@ void menu(Groups *ArrGroupe, int size)
 			cin.clear();
 			cin.ignore(10000000, '\n');
 		}
-		else
-		{
-			cin.ignore(10000000, '\n');
-		}
 		switch (k)
 		{
 		case 1:
 			menu1(ArrGroupe, size);
 			break;
 		case 2:
+			menu2(ArrGroupe, size);
 			break;
 		case 3:
 			break;
@@ -155,7 +208,7 @@ void menu(Groups *ArrGroupe, int size)
 int main()
 {
 	int size = 0;
-	Groups* ArrGroupe = new Groups[size];
+	Groups* ArrGroupe = nullptr;
 
 	menu(ArrGroupe, size);
 
